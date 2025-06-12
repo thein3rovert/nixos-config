849cae854e9c8a14510a21386164775cd5bf0b31
thein3rovert <danielolaibi@gmail.com>
2025-06-12 17:40:30 +0100

    feat(obs): OBS plugins added to programs

--- hosts/config/programs.nix
+++ hosts/config/programs.nix
@@ -7,8 +7,6 @@
 
 {
   programs.firefox.enable = true;
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = [ ];
 
   programs.hyprland = {
     enable = true;
@@ -16,5 +14,24 @@
   };
 
   programs.zsh.enable = true;
+
+  # Nix language Server
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = [ ];
+
+  # OBS Configuration
+  programs.obs-studio = {
+    enable = true;
+    enableVirtualCamera = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-composite-blur # blur effects for scenes
+      obs-vaapi # VAAPI hardware encoding
+      obs-vertical-canvas # vertical video layout support
+      obs-vkcapture # capture Vulkan applications
+      obs-webkitgtk # web sources via WebKitGTK
+      wlrobs # screen capture for Waylandgtk
+      wlrobs
+    ];
+  };
 
 }
