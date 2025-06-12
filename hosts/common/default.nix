8db899ba248032a8688300aac92a800d7bc4bd05
thein3rovert <danielolaibi@gmail.com>
2025-06-12 16:35:24 +0100

    refactor(common): Config clean up

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,5 +1,4 @@
-# Common configuration for all hosts
-
+#INFO: This file holds common configuration for all hosts
 {
   lib,
   inputs,
@@ -12,30 +11,22 @@
     ./users
     inputs.home-manager.nixosModules.home-manager
   ];
-  #Enable suer packages and make the input and oputput of the flake available to home manager
+
+  # Enable users packages and make the input and output
+  # of the flake available to home manager
   home-manager = {
     useUserPackages = true;
     extraSpecialArgs = { inherit inputs outputs; };
   };
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.stable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
+
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
@@ -51,6 +42,8 @@
         "thein3rovert"
       ]; # Set users that are allowed to use the flake command
     };
+
+    # Automatic System Cleaning
     gc = {
       automatic = true;
       options = "--delete-older-than 30d";
@@ -59,7 +52,11 @@
     registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
       (lib.filterAttrs (_: lib.isType "flake")) inputs
     );
+
+    # Nix path
     nixPath = [ "/etc/nix/path" ];
   };
+
+  #INFO: Disabled so remote vm dont inherit zsh on deployment
   # users.defaultUserShell = pkgs.zsh;
 }
