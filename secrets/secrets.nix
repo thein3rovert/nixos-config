7093dd3a395b6878f8440803daf15159a7c24876
thein3rovert <danielolaibi@gmail.com>
2025-06-21 19:59:57 +0100

    feat(Added system and system users to agenix for easy encryption and decryption):

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,14 @@
 let
+
+  # === SYSTEM ===
   nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL60abQ+uQoKBdYylRMzMbqSBKMeCj0cU9hJMT7O0gn6"; # Main system
 
+  # === USERS ===
+  thein3rovert = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKcMZafP6nbYGk5MKxll1GkI/JKesULVmHL0ragX0Qe";
+
   systems = [ nixos ];
+  users = [ thein3rovert ];
 in
 {
-  "secret1.age".publicKeys = [ nixos ];
+  "secret1.age".publicKeys = systems ++ users;
 }
