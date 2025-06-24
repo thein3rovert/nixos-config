1d88bcf161ca906c73f093e688a2058f0d901051
thein3rovert <danielolaibi@gmail.com>
2025-06-24 18:07:12 +0100

    feat(modules): Created a user modules for every host

---
+++ modules/thein3rovert/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}:
+{
+  config = lib.mkIf config.myUsers.thein3rovert.enable {
+    users.users.thein3rovert = {
+      description = "Danny thein3rovert";
+      extraGroups = config.myUsers.defaultGroups;
+      hashedPassword = config.myUsers.password;
+      isNormalUsers = true;
+
+      # openssh.authorizedKeys.keyFiles = lib.map (file: "${self.inputs.secrets}/publicKeys/${file}") (
+      #   lib.filter (file: lib.hasPrefix "aly_" file) (
+      #     builtins.attrNames (builtins.readDir "${self.inputs.secrets}/publicKeys")
+      #   )
+      # );
+      # TODO: Find and Replace ID and Keep shell as BASH
+      # shell = pkgs.zsh;
+      # uid = 1000;
+    };
+  };
+}
