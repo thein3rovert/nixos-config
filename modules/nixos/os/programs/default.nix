1c927345e3b6496bbba1a7a3c0760db3c63d2db3
thein3rovert <danielolaibi@gmail.com>
2025-08-15 21:12:48 +0100

    chore: Added obs-studio config to modules

--- modules/nixos/os/programs/default.nix
+++ modules/nixos/os/programs/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./podman
     ./vscode
+    ./obs-studio
   ];
 }
---
+++ modules/nixos/os/programs/obs-studio/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # Handled and managed by nixos services
+  options.nixosSetup.programs.obs-studio.enable = lib.mkEnableOption "OBS Studio Configuration";
+
+  config = lib.mkIf config.nixosSetup.programs.obs-studio.enable {
+    programs.obs-studio = {
+      enable = true;
+      enableVirtualCamera = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-composite-blur # blur effects for scenes
+        obs-vaapi # VAAPI hardware encoding
+        obs-vertical-canvas # vertical video layout support
+        obs-vkcapture # capture Vulkan applications
+        obs-webkitgtk # web sources via WebKitGTK
+        wlrobs # screen capture for Waylandgtk
+        wlrobs
+      ];
+    };
+  };
+}
