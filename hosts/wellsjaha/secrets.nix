b561ae7af844ef245a75306789c7280bcae571e1
thein3rovert <danielolaibi@gmail.com>
2025-08-15 14:40:53 +0100

    chore: setting up tailscale key for wellsjaha

---
+++ hosts/wellsjaha/secrets.nix
@@ -0,0 +1,9 @@
+{
+  age = {
+    tailscale = {
+      file = ../../secrets/tailscale-env.age;
+      owner = "thein3rovert";
+      path = "/home/thein3rovert/secrets/.tailscale-env";
+    };
+  };
+}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   # === SYSTEMS ===
   nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL60abQ+uQoKBdYylRMzMbqSBKMeCj0cU9hJMT7O0gn6"; # Main system
   vps-het-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFAMePGuz68j21bIQC8KospK3bs9xvTocngpY6h035Gh"; # vps system
-
+  wellsjaha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKNqDp0mblc7I7Prz/cFYf20IeMq1zvH4WlJbhp4W0h9";
   # === USERS ===
   thein3rovert-cloud = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKU41SZ5MS0hOVb79aCu7E8w7+i6rbMr7JpSXcEXbti7"; # vps user
   thein3rovert = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKcMZafP6nbYGk5MKxll1GkI/JKesULVmHL0ragX0Qe"; # Main user
@@ -10,6 +10,7 @@ let
   systems = [
     nixos
     vps-het-1
+    wellsjaha
   ];
   users = [
     thein3rovert
@@ -23,4 +24,7 @@ in
   "traefik-env.age".publicKeys = systems ++ users;
   "minio-env.age".publicKeys = systems ++ users;
   "tailscale-env.age".publicKeys = systems ++ users;
+
+  # Tailscale for wellsjaha
+  "tailscale-env-01.age".publicKeys = systems ++ users;
 }
