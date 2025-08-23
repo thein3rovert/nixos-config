2a1e7906408f4518e1c11919a5268ece3c7e1e0b
thein3rovert <danielolaibi@gmail.com>
2025-08-23 13:55:45 +0100

    feat: hosted audiobookshelf using tailscale

--- hosts/wellsjaha/audiobookShelf.nix
+++ hosts/wellsjaha/audiobookShelf.nix
@@ -1,17 +1,51 @@
 { pkgs, lib, ... }:
 {
-  virtualisation.oci-containers.containers."audiobookshelf" = {
-    image = "advplyr/audiobookshelf";
-    ports = [ "0.0.0.0:3992:80" ]; # Local port mapping
-    volumes = [
+  virtualisation.oci-containers = {
+    containers = {
 
-      # FIX: Change volume point
-      "/home/thein3rovert/audiobookshelf/config:/config:rw"
-      "/home/thein3rovert/audiobookshelf/metadata:/metadata:rw"
-      "/home/thein3rovert/audiobookshelf/media:/audiobooks:ro"
-    ];
+      "audiobookshelf-ts" = {
+        image = "tailscale/tailscale:latest";
+        hostname = "audiobooks";
+        environment = {
+          "TS_AUTHKEY" = "ts-authKey";
+          "TS_SERVE_CONFIG" = "/config/audiobookshelf.json";
+          "TS_STATE_DIR" = "/var/lib/tailscale";
+          "TS_USERSPACE" = "true";
+        };
+        volumes = [
+          "/home/thein3rovert/audiobookshelf/ts-config:/config:rw"
+          "/home/thein3rovert/audiobookshelf/ts-state:/var/lib/tailscale:rw"
+        ];
+        log-driver = "journald";
+        extraOptions = [
+          "--hostname=audiobooks"
+          "--network-alias=audiobookshelf-ts"
+        ];
+      };
 
+      "audiobookshelf" = {
+        image = "advplyr/audiobookshelf";
+        ports = [ "0.0.0.0:3992:80" ]; # Local port mapping
+        volumes = [
+
+          #FIX: Change volume point
+          "/home/thein3rovert/audiobookshelf/config:/config:rw"
+          "/home/thein3rovert/audiobookshelf/metadata:/metadata:rw"
+          "/home/thein3rovert/audiobookshelf/media:/audiobooks:ro"
+        ];
+
+        # Network mode - sharing network with another container
+        # Shares the network stack with the Tailscale container
+        extraOptions = [
+          "--network=container:audiobookshelf-ts"
+        ];
+        # Fixes dependencies issues
+        dependsOn = [ "audiobookshelf-ts" ];
+      };
+      #
+    };
   };
+
   systemd.services."podman-audiobookshelf" = {
     serviceConfig = {
       Restart = lib.mkOverride 90 "always";
@@ -23,4 +57,5 @@
       "podman-compose-audiobookshelf-root.target"
     ];
   };
+
 }
