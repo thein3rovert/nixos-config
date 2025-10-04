da2c21337c9c84f5d39664dbfae5d4407cb8093e
thein3rovert <danielolaibi@gmail.com>
2025-10-04 15:33:00 +0100

    chore: Using agenix from secret

--- hosts/wellsjaha/secrets.nix
+++ hosts/wellsjaha/secrets.nix
@@ -1,15 +1,22 @@
+{ self, ... }:
 {
   age = {
     secrets = {
-      tailscale = {
-        file = ../../secrets/tailscale-env-01.age;
-        owner = "thein3rovert";
-        path = "/home/thein3rovert/secrets/.tailscale-env";
-      };
-      audiobookshelf = {
-        file = ../../secrets/audiobookshelf-env.age;
+      # tailscale = {
+      #   file = ../../secrets/tailscale-env-01.age;
+      #   owner = "thein3rovert";
+      #   path = "/home/thein3rovert/secrets/.tailscale-env";
+      # };
+
+      # audiobookshelf = {
+      #   file = ../../secrets/audiobookshelf-env.age;
+      #   # owner = "thein3rovert";
+      #   path = "/home/thein3rovert/secrets/.audiobookshelf-env";
+      # };
+      linkding = {
+        file = "${self.inputs.secrets}/linkding-env.age";
         # owner = "thein3rovert";
-        path = "/home/thein3rovert/secrets/.audiobookshelf-env";
+        path = "/home/thein3rovert/secrets/.linkding-env";
       };
     };
   };
