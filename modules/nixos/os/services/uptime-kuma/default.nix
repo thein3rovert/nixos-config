2b744b1e77950a1148b9d8474390d011da17e4b0
thein3rovert <danielolaibi@gmail.com>
2025-11-02 22:37:54 +0000

    chore(monitoring): uptime kuma added

---
+++ modules/nixos/os/services/uptime-kuma/default.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+
+  if-uptime-kuma-enable = lib.mkIf config.nixosSetup.services.uptime-kuma.enable;
+  imageName = "louislam/uptime-kuma";
+  host = "127.0.0.1";
+  port = 8380;
+  dataVolume = "uptime-kuma_data";
+in
+{
+  options.nixosSetup.services.uptime-kuma = {
+    enable = lib.mkEnableOption "Monitoring";
+  };
+
+  config = if-uptime-kuma-enable {
+
+    myContainers.traefik = {
+      enable = true;
+      defaultEntryPoints = [ "websecure" ];
+      defaultCertResolver = "godaddy";
+    };
+    myContainers = {
+      enable = true;
+      containers = {
+        uptime-kuma = {
+          image = "${imageName}";
+          ports = [ "${host}:${toString port}:8380" ];
+          volumes = [
+            "${dataVolume}:/app/data"
+
+          ];
+          traefik = {
+            enable = true;
+            url = "http://localhost:8380/";
+            rule = "Host(`uptime-kuma.thein3rovert.dev`)";
+          };
+
+        };
+      };
+    };
+  };
+}
