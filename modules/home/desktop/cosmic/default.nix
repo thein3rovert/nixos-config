3c894760a1ff789f59ffbc59ed5b9349a9af14b3
thein3rovert <danielolaibi@gmail.com>
2025-11-18 16:37:46 +0000

    feat: Added two destop enviroment from aly

---
+++ modules/home/desktop/cosmic/default.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.homeSetup.desktop.cosmic.enable = lib.mkEnableOption "COSMIC desktop environment";
+
+  config = lib.mkIf config.homeSetup.desktop.cosmic.enable {
+    myHome.profiles.defaultApps = {
+      audioPlayer.package = lib.mkDefault pkgs.gapless;
+      editor.package = lib.mkDefault pkgs.cosmic-edit;
+      fileManager.package = lib.mkDefault pkgs.cosmic-files;
+      imageViewer.package = lib.mkDefault pkgs.loupe;
+      pdfViewer.package = lib.mkDefault pkgs.papers;
+      terminal.package = lib.mkDefault pkgs.cosmic-term;
+      videoPlayer.package = lib.mkDefault pkgs.cosmic-player;
+    };
+
+    programs = {
+      cosmic-ext-ctl.enable = true;
+      cosmic-ext-tweaks.enable = true;
+      cosmic-manager.enable = true;
+    };
+  };
+}
