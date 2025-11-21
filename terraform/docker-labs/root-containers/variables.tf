0e27b60d3c12606542d0da90bb91e8e15ca215b8
thein3rovert <danielolaibi@gmail.com>
2025-11-21 19:38:00 +0000

    chore: create separate module for both nginx container

---
+++ terraform/docker-labs/root-containers/variables.tf
@@ -0,0 +1,15 @@
+variable "nginx_host" {
+  description = "Nginx host configuration"
+  type        = string
+  default     = "localhost"
+}
+
+variable "nginx_port" {
+  description = "Nginx port configuration"
+  type        = string
+  default     = "80"
+}
+
+
+
+
---
+++ terraform/docker-labs/user-containers/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  # Fixed issues with network access
+  host ="unix:///run/user/1000/podman/podman.sock"   # Root socket instead of user
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "web" {
+  image = docker_image.nginx.image_id
+  name  = "my-nginx-user-container"
+  
+  # networks_advanced {
+  #   name = "traefik_proxy"  # Connect to the Traefik network
+  # }
+
+  # network_mode = "host" this was causing the conflict
+  network_mode = "host"
+
+  # Configure nginx to listen on port 8082 instead of 80
+  command = [
+    "sh", "-c", 
+    "sed -i 's/listen.*80;/listen 8082;/' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
+  ]
+    labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+  
+  labels {
+    label = "traefik.http.routers.nginx.rule"
+    value = "Host(`nginx-console.l.thein3rovert.com`)"
+  }
+  
+  labels {
+    label = "traefik.http.services.nginx.loadbalancer.server.port"
+    value = "8082"
+  }
+  
+  restart = "unless-stopped"
+}
