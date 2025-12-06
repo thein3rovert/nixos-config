374686df28aabf19c9a3e1870753213e8c4d25ad
thein3rovert <danielolaibi@gmail.com>
2025-12-06 15:00:01 +0000

    chore: bootstrap and create images and containers

---
+++ modules/bootstrap/flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Manage NixOS server remotely";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    colmena.url = "github:zhaofengli/colmena";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      disko,
+      colmena,
+      nixos-generators,
+      ...
+    }:
+    {
+      nixosConfigurations.demo = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./configuration.nix
+        ];
+      };
+
+      packages.x86_64-linux = {
+        lxc = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            ./configuration.nix
+          ];
+          format = "proxmox-lxc";
+        };
+      };
+
+      colmenaHive = colmena.lib.makeHive {
+        meta = {
+          nixpkgs = nixpkgs.legacyPackages.x86_64-linux;
+        };
+        defaults =
+          { pkgs, ... }:
+          {
+            environment.systemPackages = [
+              pkgs.curl
+            ];
+          };
+        finn =
+          { pkgs, ... }:
+          {
+            deployment = {
+              targetHost = "finn";
+              targetPort = 22;
+              targetUser = "thein3rovert";
+              buildOnTarget = true;
+              tags = [ "homelab" ];
+            };
+            nixpkgs.system = "x86_64-linux";
+            imports = [
+              disko.nixosModules.disko
+              ./configuration.nix
+            ];
+            time.timeZone = "Europe/London";
+          };
+      };
+    };
+}
