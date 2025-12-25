76d59b5fd0a20f16f4889cf2bc552ba8da37832d
thein3rovert <danielolaibi@gmail.com>
2025-12-25 14:52:22 +0000

    chore: add ssh config for main user

--- modules/nixos/users/thein3rovert/default.nix
+++ modules/nixos/users/thein3rovert/default.nix
@@ -3,8 +3,20 @@
   lib,
   inputs,
   pkgs,
+  self,
   ...
 }:
+let
+  inherit (lib)
+    map
+    filter
+    hasPrefix
+    ;
+
+  createMap = map;
+  filterOut = filter;
+  containThePrefix = hasPrefix;
+in
 {
   config = lib.mkIf config.myUsers.thein3rovert.enable {
     users.users.thein3rovert = {
@@ -17,6 +29,7 @@
       # Fixes home-manager not showing after enabled error
       packages = [ inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.default ];
 
+      # Management
       openssh.authorizedKeys.keys = [
         ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObli1unUWlbZaja5VMzTIvPBJOCI/E6vs/qhrVkSHLO thein3rovert''
       ];
@@ -24,12 +37,11 @@
       # === Enable ssh config when installed ===
       # shell = pkgs.zsh;
 
-      # === KISS ==
-      # openssh.authorizedKeys.keyFiles = lib.map (file: "${self.inputs.secrets}/publicKeys/${file}") (
-      #   lib.filter (file: lib.hasPrefix "aly_" file) (
-      #     builtins.attrNames (builtins.readDir "${self.inputs.secrets}/publicKeys")
-      #   )
-      # );
+      openssh.authorizedKeys.keyFiles = createMap (file: "${self.inputs.secrets}/publicKeys/${file}") (
+        filterOut (file: containThePrefix "thein3rovert_" file) (
+          builtins.attrNames (builtins.readDir "${self.inputs.secrets}/publicKeys")
+        )
+      );
 
     };
 
