70212deb86d8bdc0965525a0afdfd892ef8d871b
thein3rovert <danielolaibi@gmail.com>
2026-01-12 21:09:17 +0000

    chore: ubuntu lxc successful provisioning

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -14,15 +14,29 @@ provider "proxmox" {
   pm_api_token_secret = "0f955002-e7ec-4290-b930-22dd85e536ba"
 }
 
-module "ubuntu_vm" {
-  source = "../../modules/cloud-init"
+#module "ubuntu_vm" {
+#  source = "../../modules/cloud-init"
+#  
+#  vm_name       = "ubuntu-test"
+#  target_node   = "thein3rovert"
+#  template_name = "ubuntu-cloud-template"
+#  cores         = 2
+#  memory        = 2048
+#  disk_size     = "15G"
+#  storage       = "local-lvm"
+#  ssh_keys      = file("/home/thein3rovert/.ssh/id_ed25519.pub")
+#}
+
+
+module "ubuntu_container" {
+  source = "../../modules/lxc-container"
   
-  vm_name       = "ubuntu-test"
-  target_node   = "thein3rovert"
-  template_name = "ubuntu-cloud-template"
-  cores         = 2
-  memory        = 2048
-  disk_size     = "15G"
-  storage       = "local-lvm"
+  target_node = "thein3rovert"
+  hostname    = "ubuntu-ct-01"
+  template    = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  cores       = 2
+  memory      = 2048
+  disk_size   = "8G"
+  storage     = "local-lvm"
   ssh_keys      = file("/home/thein3rovert/.ssh/id_ed25519.pub")
 }
