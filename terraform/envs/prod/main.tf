02a07819a9f0dd1c92b5d734ba9574d0369b8dd0
thein3rovert <danielolaibi@gmail.com>
2026-01-17 19:58:04 +0000

    chore: update variabl for prod

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -3,7 +3,7 @@ terraform {
     proxmox = {
       source  = "telmate/proxmox"
       version = "3.0.2-rc07" # Using the required version
-}
+    }
   }
 }
 
@@ -14,15 +14,17 @@ provider "proxmox" {
   pm_api_token_secret = "0f955002-e7ec-4290-b930-22dd85e536ba"
 }
 
-module "ubuntu_container" {
-  source = "../../modules/lxc"
-  
-  target_node = "thein3rovert"
-  hostname    = "ubuntu-ct-02"
-  template    = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-  cores       = 2
-  memory      = 2048
-  disk_size   = "8G"
-  storage     = "local-lvm"
-  ssh_keys      = file("/home/thein3rovert/.ssh/id_ed25519.pub")
-}
+# module "ubuntu_container" {
+#   source = "../../modules/lxc"
+#
+#   target_node = "thein3rovert"
+#   hostname    = "ubuntu-ct-02"
+#   template    = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+#   cores       = 2
+#   memory      = 2048
+#   disk_size   = "8G"
+#   storage     = "local-lvm"
+#   ssh_keys    = file("/home/thein3rovert/.ssh/id_ed25519.pub")
+# }
+
+
--- terraform/envs/prod/variables.tf
+++ terraform/envs/prod/variables.tf
@@ -19,41 +19,87 @@ variable "container_id" {
   description = "The ID for the LXC container. If null, Proxmox will assign the next available ID."
   default     = "110"
   validation {
-    condition     = var.ip_prefix == null || (var.ip_prefix != null && var.container_id != null)
+    condition     = var.ip_base == null || (var.ip_base != null && var.container_id != null)
     error_message = "If you specify an 'ip_prefix', you must also specify a 'container_id'."
   }
 }
+
 variable "hostname" {
   type        = string
   description = "The hostname for the new LXC container."
-  default     = "thein3rovert-iac""
+  default     = "thein3rovert-iac"
 }
-variable "ip_prefix" {
+
+variable "ip_base" {
   type        = string
-  description = "The IP prefix for a static IP (e.g., '192.168.0'), setting a container_id is required for this. If null, DHCP will be used."
-  default     = "192.168.0"
+  description = "The IP prefix for a static IP (e.g., '10.20.0'), setting a container_id is required for this. If null, DHCP will be used."
+  default     = "10.10.20.0"
 }
+
 variable "cidr_suffix" {
   type        = string
   description = "The CIDR suffix for the network (e.g., '24' for /24)."
   default     = "24"
 }
+
 variable "gateway" {
   type        = string
-  description = "The network gateway IP address. Required for static IP configuration."
-  default     = "192.168.0.1"
+  description = "The network gateway IP address. Required for static IP configuration. (e.g., 10.10.20.1)"
+  default     = "10.10.20.1"
 }
+
+# Create local for gateway and ip-prefix
+#   locals {
+#   ip_prefix = "${var.ip_base}/${var.cidr_suffix}"
+# }
+# locals {
+#   gateway = cidrhost(var.ip_prefix, 1)
+# }
+
 variable "target_node" {
   type        = string
   description = "The name of the target Proxmox node."
+  default     = "thein3rovert"
 }
-variable "ostemplate" {
-  type        = string
-  description = "The name of the LXC template to use (e.g., 'local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz')."
-  default     = "local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz"
+# variable "ostemplate" {
+#   type        = string
+#   description = "The name of the LXC template to use (e.g., 'local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz')."
+#   default     = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+#   # default     = "local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz"
+# }
+
+variable "ostemplate_choice" {
+  description = "Choose an OS template: 1 for Ubuntu, 2 for Alpine"
+  type        = number
+  default     = 1
+  validation {
+    condition     = contains([1, 2], var.ostemplate_choice)
+    error_message = "Valid choices: 1 (Ubuntu), 2 (Alpine)"
+  }
 }
+locals {
+  ostemplate = (
+    var.ostemplate_choice == 1
+    ? "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+    : "local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz"
+  )
+}
+
 variable "ssh_public_key_path" {
   type        = string
   description = "The path to the public SSH key to install in the container."
   default     = "~/.ssh/id_ed25519.pub"
 }
+
+# variable "ostemplate" {
+#   type        = string
+#   description = "The name of the LXC template to use."
+#   default     = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+#   validation {
+#     condition     = contains([
+#       "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst",
+#       "local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz"
+#     ], var.ostemplate)
+#     error_message = "ostemplate must be either Ubuntu 22.04 or Alpine 3.22."
+#   }
+# }
