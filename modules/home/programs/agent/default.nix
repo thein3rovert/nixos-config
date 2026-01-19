3bf1ab41a4459602f5b255a53cc218624cfbe07a
thein3rovert <danielolaibi@gmail.com>
2026-01-19 20:24:32 +0000

    chore: open code agent config

--- modules/home/programs/agent/default.nix
+++ modules/home/programs/agent/default.nix
@@ -1,31 +1,68 @@
-{ pkgs, ... }:
 {
-  imports = [
-    ./opencode.nix
-  ];
-  home.packages = with pkgs; [
-    # agenix-cli
-    # beads
-    # bc
-    bun
-    # claude-code
-    # devpod
-    #devpod-desktop
-    # code2prompt
-    # nur.repos.charmbracelet.crush
-    (python3.withPackages (
-      ps: with ps; [
-        pip
-        # uv
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.homeSetup.programs.agent.enable =
+    lib.mkEnableOption "Base AI agent for with opencode experiment";
 
-        # Scientific packages
-        # numba
-        # numpy
-        # torch
-        # srt
-      ]
-    ))
-    nixd
-    alejandra
-  ];
+  config = lib.mkIf config.homeSetup.programs.eza.enable {
+    home.packages = with pkgs; [
+      bun
+      (python3.withPackages (
+        ps: with ps; [
+          pip
+        ]
+      ))
+    ];
+
+    programs.agent = {
+      enable = true;
+      settings = {
+        theme = "opencode";
+        plugin = [ "oh-my-opencode" ];
+        formatter = {
+          alejandra = {
+            command = [
+              "alejandra"
+              "-q"
+              "-"
+            ];
+            extensions = [ ".nix" ];
+          };
+        };
+
+        provider = {
+          github-copilot = {
+            models = {
+              "gpt-4.1" = {
+                name = "GPT-4.1";
+                limit = {
+                  context = 128000;
+                  output = 16384;
+                };
+                modalities = {
+                  input = [ "text" ];
+                  output = [ "text" ];
+                };
+              };
+              "claude-sonnet-4-5" = {
+                name = "Claude Sonnet 4.5";
+                limit = {
+                  context = 128000;
+                  output = 16000;
+                };
+                modalities = {
+                  input = [ "text" ];
+                  output = [ "text" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 }
