b92b2ce7c7c7750271ab65c83c9e8ffc86ef6c0f
thein3rovert <danielolaibi@gmail.com>
2026-01-20 16:51:34 +0000

    chore: added home-manager configuration for marcus

--- modules/flake/home-manager.nix
+++ modules/flake/home-manager.nix
@@ -1,5 +1,27 @@
-_: {
+{ self, ... }:
+{
   flake = {
+    homeConfigurations = {
+      "thein3rovert@marcus" = self.inputs.home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = { inherit self; };
+
+        modules = [
+          ../../homes/thein3rovert/marcus.nix
+        ];
+
+        pkgs = import self.inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+
+          overlays = [
+            self.inputs.nixgl.overlay
+            self.inputs.nur.overlays.default
+            self.overlays.default
+          ];
+        };
+      };
+    };
+
     homeManagerModules = {
       thein3rovert = import ../../homes/thein3rovert;
       # thein3rovert-cloud = ../home/thein3rovert-cloud;
