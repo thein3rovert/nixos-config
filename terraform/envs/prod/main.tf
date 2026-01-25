3924065cb057ef216b197dfad64e55cef98186b5
thein3rovert <danielolaibi@gmail.com>
2026-01-25 14:16:13 +0000

    chore: opencode lxc config

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -49,4 +49,30 @@ module "ubuntu_container" {
 
 }
 
+module "nixos_container_02" {
+  source = "../../modules/lxc"
+
+  target_node = var.target_node
+  password    = var.root_password
+  hostname    = "agent"
+  vmid        = var.container_id
+  ostemplate  = "local:vztmpl/nixos-image-lxc-proxmox-26.05.20251205.f61125a-x86_64-linux.tar.xz"
+  cores       = 2
+  memory      = 2048
+  swap        = 512
+  disk_size   = "8G"
+  storage     = var.rootfs_storage
+  ssh_keys    = file(var.ssh_public_key_path)
+
+  # Override of default
+  gateway         = var.gateway
+  cidr_suffix     = var.cidr_suffix
+  ip_base         = var.ip_base
+  bridge          = var.bridge
+  container_id    = 102
+  proxmox_host_ip = var.proxmox_host_ip
+  extra_tags      = "[nixos]"
+
+}
+
 
