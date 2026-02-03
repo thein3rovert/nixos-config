06f1ba517c1258a942a5ddff5b69c5918f937b1a
thein3rovert <danielolaibi@gmail.com>
2026-02-03 19:33:49 +0000

    chore: memo note taking added

---
+++ modules/nixos/os/services/memos/defailt.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  imageName = "neosmemo/memos";
+  imageTag = "stable";
+  port = 5230;
+  dataVolume = "/var/lib/memos:/var/opt/memos";
+  cfg = config.nixosSetup.services.memos;
+in
+{
+  options.nixosSetup.services.memos.enable = lib.mkEnableOption "Notes and Idea taking tool";
+
+  config = lib.mkIf cfg.enable {
+
+    virtualisation.oci-containers.containers.memos = {
+      image = "${imageName}:${imageTag}";
+      ports = [ "${toString port}:5230" ];
+
+      # TODO: ADD PATH UNDER NETWORKING BASE MODULES
+      volumes = [ dataVolume ];
+    };
+  };
+}
