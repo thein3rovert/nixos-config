e60806b1bf39ea89e0fb210cac17cf40591e9000
thein3rovert <danielolaibi@gmail.com>
2026-02-07 21:19:15 +0000

    chore: remove wsl config, not in use

--- modules/flake/home-manager.nix
+++ modules/flake/home-manager.nix
@@ -8,23 +8,23 @@
 
     # INFO: Standalone home-manager configurations for non-NixOS systems
     # Use with: home-manager switch --flake .#thein3rovert@wsl
-    homeConfigurations = {
-      "thein3rovert@wsl" = self.inputs.home-manager.lib.homeManagerConfiguration {
-        extraSpecialArgs = { inherit self; };
-
-        modules = [
-          ../../homes/thein3rovert/wsl.nix
-        ];
-
-        pkgs = import self.inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-
-          overlays = [
-            self.overlays.default
-          ];
-        };
-      };
-    };
+    # homeConfigurations = {
+    #   "thein3rovert@wsl" = self.inputs.home-manager.lib.homeManagerConfiguration {
+    #     extraSpecialArgs = { inherit self; };
+    #
+    #     modules = [
+    #       ../../homes/thein3rovert/wsl.nix
+    #     ];
+    #
+    #     pkgs = import self.inputs.nixpkgs {
+    #       system = "x86_64-linux";
+    #       config.allowUnfree = true;
+    #
+    #       overlays = [
+    #         self.overlays.default
+    #       ];
+    #     };
+    #   };
+    # };
   };
 }
