7067c9404b7e806fc81693b489bd8eafdc4042aa
thein3rovert <danielolaibi@gmail.com>
2026-03-20 22:21:09 +0000

    chore: control plan tf config added

--- terraform/modules/container/kubernetes/k3s/main.tf
+++ terraform/modules/container/kubernetes/k3s/main.tf
@@ -116,3 +116,21 @@ locals {
   server_node_token = regex(".*::server:.*", local.raw_server_node_token)
   other_control_plane_server_ips = slice(var.control_plane_ips, 1, length(var.control_plane_ips))
 }
+
+# Install and join other control plane nodes to the cluster
+resource "ssh_resource" "install_k3s_control_plane" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  count = length(local.other_control_plane_server_ips)
+
+  host         = local.other_control_plane_server_ips[count.index]
+  user         = var.ssh_user
+  private_key  = file("${var.ssh_pub_key_file_path}")
+
+  commands = [
+     "curl -fL https://get.k3s.io | sh -s - server --token ${local.server_node_token} --cluster-init --server https://${var.control_plane_ips[0]}:6443 --tls-san ${var.kube_api_loadbalancer_dns_name}"
+  ]
+  depends_on = [ ssh_resource.install_k3s_server ]
+}
