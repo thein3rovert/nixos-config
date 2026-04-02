60317cb8f20400f49eec36e8f9f79a29ba276b02
thein3rovert <danielolaibi@gmail.com>
2026-04-02 19:57:10 +0100

    chore: mnage config with nixos

--- modules/nixos/services/storage/fileshare/default.nix
+++ modules/nixos/services/storage/fileshare/default.nix
@@ -21,12 +21,43 @@ in
   };
 
   config = if-fileshare-enable {
+    # Create data directory and config file
+    systemd.tmpfiles.rules = [
+      "d /var/lib/filebrowser/data 0755 root root -"
+      "d /var/lib/filebrowser/data/tmp 0755 root root -"
+      "d /var/lib/filebrowser/files 0755 root root -"
+    ];
+
+    environment.etc."filebrowser/config.yaml".text = ''
+      server:
+        database: "data/database.db"
+        cacheDir: "data/tmp"
+        sources:
+          - path: "/files"
+            name: Home
+            config:
+              defaultUserScope: "/"
+              defaultEnabled: true
+              createUserDir: false
+        maxArchiveSize: 50
+      auth:
+        tokenExpirationHours: 2
+        methods:
+          password:
+            enabled: true
+            minLength: 5
+            signup: false
+        adminUsername: thein3rovert
+        adminPassword: filebrowser123
+    '';
+
     virtualisation.oci-containers.containers.fileshare = {
       image = "${imageName}";
-      ports = [ "${toString port}:3000" ];
+      ports = [ "${toString port}:80" ];
       volumes = [
         dataVolume
         filesVolume
+        "/etc/filebrowser/config.yaml:/home/filebrowser/data/config.yaml:ro"
       ];
       environment = {
         FILEBROWSER_CONFIG = "data/config.yaml";
