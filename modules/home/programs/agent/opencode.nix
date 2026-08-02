db77240ef46f8d9088eb643d97895335d1f28bde
thein3rovert <danielolaibi@gmail.com>
2026-08-02 21:42:37 +0100

    feat: add opencode config that renders files and folder from agent share repo into default dir
    - Add the config to get for example opencode agent requires folder and
    files and symlink them to the default opencode dir under
    .config/opencode.

--- modules/home/programs/agent/opencode.nix
+++ modules/home/programs/agent/opencode.nix
@@ -25,4 +25,40 @@
 
       modelOverrides = shared.mkModelOverridesOption;
     };
+
+  config =
+    with lib;
+    # let
+    #
+    #   shared = ./shared/common.nix { inherit lib; };
+    #   cfg = config.ai.agents.opencode;
+    # in
+
+    mkIf cfg.enable {
+      # Render agent files and symlinked to opencode config in .config
+      xdg.configFile."opencode/agents" =
+        let
+          allAgentLib = (import ../../../../lib { inherit lib; }).agents;
+        in
+        mkIf (cfg.agentsInput != null) {
+          source = allAgentLib.renderForOpencode {
+            inherit pkgs;
+            # INFO: Still dont know what load agent is yet
+            canonical = cfg.agentsInput.lib.loadAgents;
+            modelOverrides = cfg.modelOverrides;
+          };
+        };
+
+      # Static config dirs from POLIS ( Shared Agent repo )
+      xdg.configFile."opencode/context" = mkIf (cfg.agentsInput != null) {
+        sources = "${cfg.agentsInput}/context";
+      };
+      xdg.configFile."opencode/commands" = mkIf (cfg.agentsInput != null) {
+        source = "${cfg.agentsInput}/commands";
+      };
+      xdg.configFile."opencode/prompts" = mkIf (cfg.agentsInput != null) {
+        source = "${cfg.agentsInput}/prompts";
+      };
+    };
+
 }
