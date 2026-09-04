b26eaa82f0b3d6041c5433776b5f6b52c63bdb27
thein3rovert <danielolaibi@gmail.com>
2026-09-04 19:11:38 +0100

    chore: add colmena config for nightblood and change buildonTarget -> false for..
    roan
    finn
    nightblood
    Because they dont have enough resources

--- flake.nix
+++ flake.nix
@@ -429,7 +429,7 @@
                 targetHost = "192.168.0.51";
                 targetPort = 22;
                 targetUser = "thein3rovert";
-                buildOnTarget = true;
+                buildOnTarget = false;
                 tags = [ "homelab" ];
               };
               nixpkgs.system = "x86_64-linux";
@@ -442,14 +442,36 @@
                 self.nixosModules.nixosOs
               ];
             };
-            # ---- Node: Roan [ lxc 02 ] ----
+            # ---- Node: Roan [ lxc 03] ----
             roan = {
               deployment = {
                 targetHost = "192.168.0.103";
                 targetPort = 22;
                 targetUser = "thein3rovert";
-                buildOnTarget = true;
+                buildOnTarget = false;
                 tags = [ "homelab" ];
+              };
+              nixpkgs.system = "x86_64-linux";
+              imports = [
+                ./hosts/roan
+                agenix.nixosModules.default
+                self.nixosModules.base
+                # self.inputs.home-manager.nixosModules.home-manager
+                self.nixosModules.nixosOs
+              ];
+            };
+
+            # ---- Node: NIghtblood [ lxc 04] ----
+            nightblood = {
+              deployment = {
+                targetHost = "192.168.0.104";
+                targetPort = 22;
+                targetUser = "thein3rovert";
+                buildOnTarget = false;
+                tags = [
+                  "homelab"
+                  "nas_storage"
+                ];
               };
               nixpkgs.system = "x86_64-linux";
               imports = [
