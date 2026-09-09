cb439264e120deba4dd86c8023007877163d72f9
thein3rovert <danielolaibi@gmail.com>
2026-09-09 16:22:49 +0100

    chore: removing old vps host

--- hosts/vps-het-1/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  imports = [
-    ./services
-    ./configuration.nix
-    ./shell.nix # INFO: TEMPORARY WHILE I CLEAN UP
-    # ./secrets.nix
-    ./home.nix
-  ];
-}
--- hosts/vps-het-1/services/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  imports = [
-    #TODO: Enable when age is back up
-    # ./containers
-    # ./podman.nix
-    # ./traefik.nix
-    # ./postgres.nix
-  ];
-}
